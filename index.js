9466b4120768d49959f903eedc27d207146d004c
Yun-Chi Hsiao <is0.jimhsiao@gmail.com>
2021-11-05 12:42:23 +0800

    Update: Test passed

--- index.js
+++ index.js
@@ -1,1 +1,26 @@
-// Code your solution here
+function findMatching(drivers, string) {
+	
+	let res = drivers.filter(d => {
+		return d.toLowerCase() === string.toLowerCase();
+	})
+
+	return res;
+}
+
+function fuzzyMatch(drivers, string) {
+
+	let res = drivers.filter(d => {
+		return string.includes(d[0]);
+	})
+
+	return res;
+}
+
+function matchName(drivers, string) {
+	
+	let res = drivers.filter(d => {
+		return d["name"].toLowerCase() === string.toLowerCase();
+	})
+
+	return res;
+}
